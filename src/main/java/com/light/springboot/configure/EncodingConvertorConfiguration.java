9c0bdc43f93f8e7f688b7bb6ce22fc68c77ac6f2
stonemonkey2016 <stonemonkey@aliyun.com>
2019-12-27 16:20:33 +0800

    《Spring Boot 入门之基础篇（一）》

---
+++ src/main/java/com/light/springboot/configure/EncodingConvertorConfiguration.java
@@ -0,0 +1,25 @@
+package com.light.springboot.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @descrption:
+ * @author: bubbles
+ */
+@Configuration
+public class EncodingConvertorConfiguration {
+
+    @Bean
+    @Conditional(UTF8Condition.class)
+    public EncodingConvertor createUTF8EncodingConvertor(){
+        return new UTF8EncodingConvertor();
+    }
+
+    @Bean
+    @Conditional(GBKCondition.class)
+    public EncodingConvertor createGBKEncodingConvertor(){
+        return new GBKEncodingConvertor();
+    }
+}
